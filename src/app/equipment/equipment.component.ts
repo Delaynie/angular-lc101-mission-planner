7729db3f28ef0d36b2d426ed5e44b0a8db5c431b
Delaynie <apple@Delaynies-MBP.attlocal.net>
2021-01-08 15:58:09 -0600

    finished equipment components

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -28,8 +28,8 @@ export class EquipmentComponent implements OnInit {
 
    // Code your addItem function here:
    addItem(item : object) : boolean {
-     this.cargoHold.push(item);
-     this.cargoMass += item['mass'];
-     return this.maximumAllowedMass - this.cargoMass <= 200;
-   }
+    this.cargoHold.push(item);
+    this.cargoMass += item['mass'];
+    return this.maximumAllowedMass - this.cargoMass <= 200;
+ }
 }
